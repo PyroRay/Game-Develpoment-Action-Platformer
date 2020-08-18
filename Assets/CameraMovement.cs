767c77e1f546a3cb709beb8ff6f7295a745b2a3e
Ray Peng <ygnihTtaC@users.noreply.github.com>
2020-08-17 20:12:35 -0700

    Updated Camera
    Camera now follows player. Will probably change how this works later on as it is a bit too simple.

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float offsetX = 0;
+    public float offsetY = 0;
+    private Transform playerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2(playerTransform.position.x + offsetX, playerTransform.position.y + offsetY);
+    }
+}
